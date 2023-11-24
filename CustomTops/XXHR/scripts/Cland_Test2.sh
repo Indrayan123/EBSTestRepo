1e0a223eac29edba514110d7236e0385e48b73c6
Indrayan Chakraborty <indrayan.chakraborty@flexagon.com>
2023-11-24 12:10:58 +0530

    RFC - 104 C Land File changed by Indrayan C

--- CustomTops/XXHR/scripts/Cland_Test2.sh
+++ CustomTops/XXHR/scripts/Cland_Test2.sh
@@ -1,7 +1,7 @@
-echo "This is ninth version -2 of second file";
+echo "This is elevent version -2 of second file - Indrayan Cha";
 echo "Second line";
-echo "33 line";
+echo "34 line";
 echo "Forth line";
 echo "61 line";
 echo "51 line";
-echo "81 line";
+echo "91 line";
