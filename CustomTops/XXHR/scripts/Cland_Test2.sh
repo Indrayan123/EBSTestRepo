b2396459c5b38a75085349a883ecf68c871e756a
Indrayan Chakraborty <indrayan.chakraborty@gmail.com>
2023-11-24 12:09:02 +0530

    RFC 103:File version 101-Ind123 modified

--- CustomTops/XXHR/scripts/Cland_Test2.sh
+++ CustomTops/XXHR/scripts/Cland_Test2.sh
@@ -1,7 +1,7 @@
-echo "This is ninth version -2 of second file";
+echo "This is tenth version -2 of second file - Indrayn123";
 echo "Second line";
-echo "33 line";
+echo "34 line";
 echo "Forth line";
 echo "61 line";
 echo "51 line";
-echo "81 line";
+echo "71 line";
